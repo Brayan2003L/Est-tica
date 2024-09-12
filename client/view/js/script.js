2d244d424a78800573cf145aaba140cd1c72af1e
Brayan2003L <brayanlesmes2003@gmail.com>
2024-09-12 11:47:22 -0500

    Agregando carpeta 'client'

---
+++ client/view/js/script.js
@@ -0,0 +1,16 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const toggleButton = document.getElementById('toggle-button');
+    const hiddenRows = document.querySelectorAll('.appointment-row.hidden');
+
+    let rowsVisible = false;
+
+    toggleButton.addEventListener('click', function() {
+        rowsVisible = !rowsVisible;
+
+        hiddenRows.forEach(row => {
+            row.classList.toggle('hidden', !rowsVisible);
+        });
+
+        toggleButton.textContent = rowsVisible ? 'Mostrar menos' : 'Mostrar más';
+    });
+});
